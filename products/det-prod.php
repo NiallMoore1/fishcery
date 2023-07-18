24b998f32038257ec87a9e1823a4f084889a2ebb
NiallMoore <moore.nialls@gmail.com>
2023-07-18 17:21:30 +0100

    setup part 1

--- products/det-prod.php
+++ products/det-prod.php
@@ -1,10 +1,24 @@
 <?php require "../includes/header.php"; ?>
 <?php require "../config/config.php";  ?>
+<?php 
 
 
+if(isset($_GET['id'])) {
+
+    $id = $_GET['id'];
+
+    $select = $conn->query("SELECT * FROM products WHERE status = 1 AND id='$id'");
+    $select->execute();
+
+    $product = $select->fetch(PDO::FETCH_OBJ);
+    } else {
+
+    }
+
+?>
     <div id="page-content" class="page-content">
         <div class="banner">
-            <div class="jumbotron jumbotron-bg text-center rounded-0" style="background-image: url('assets/img/bg-header.jpg');">
+            <div class="jumbotron jumbotron-bg text-center rounded-0" style="background-image: url('<?php echo APPURL;?>/assets/img/bg-header.jpg');">
                 <div class="container">
                     <h1 class="pt-5">
                         The Meat Product Title
@@ -20,27 +34,22 @@
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="slider-zoom">
-                            <a href="assets/img/meats.jpg" class="cloud-zoom" rel="transparentImage: 'data:image/gif;base64,R0lGODlhAQABAID/AMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==', useWrapper: false, showTitle: false, zoomWidth:'500', zoomHeight:'500', adjustY:0, adjustX:10" id="cloudZoom">
-                                <img alt="Detail Zoom thumbs image" src="assets/img/meats.jpg" style="width: 100%;">
+                            <a href="<?php echo APPURL;?>/assets/img/<?php echo $product->image; ?>" class="cloud-zoom" rel="transparentImage: 'data:image/gif;base64,R0lGODlhAQABAID/AMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==', useWrapper: false, showTitle: false, zoomWidth:'500', zoomHeight:'500', adjustY:0, adjustX:10" id="cloudZoom">
+                                <img alt="Detail Zoom thumbs image" src="<?php echo $product->image; ?>" style="width: 100%;">
                             </a>
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <p>
                             <strong>Overview</strong><br>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            <?php echo $product->description; ?>
                         </p>
                         <div class="row">
                             <div class="col-sm-6">
                                 <p>
                                     <strong>Price</strong> (/Pack)<br>
-                                    <span class="price">Rp 100.000</span>
-                                    <span class="old-price">Rp 150.000</span>
+                                    <span class="price">€<?php echo $product->price; ?></span>
+                                    <!--<span class="old-price">Rp 150.000</span> -->
                                 </p>
                             </div>
                            
@@ -50,7 +59,7 @@
                         </p>
                         <div class="row">
                             <div class="col-sm-5">
-                                <input class="form-control" type="number" min="1" data-bts-button-down-class="btn btn-primary" data-bts-button-up-class="btn btn-primary" value="1" name="vertical-spin">
+                                <input class="form-control" type="number" min="1" data-bts-button-down-class="btn btn-primary" data-bts-button-up-class="btn btn-primary" value="<?php echo $product->quantity; ?>" name="vertical-spin">
                             </div>
                             <div class="col-sm-6"><span class="pt-1 d-inline-block">Pack (1000 gram)</span></div>
                         </div>
@@ -242,7 +251,8 @@
     </div>
 
     <?php require "../includes/footer.php" ;?>
-    
+
+
     <script>
         $(document).ready(function() {
             $(".form-control").keyup(function(){
@@ -253,3 +263,4 @@
 
         })
     </script>
+   
