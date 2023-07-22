e04bd18b5d00da6f72ecb7455794d58806cc0929
NiallMoore <moore.nialls@gmail.com>
2023-07-22 17:05:38 +0100

    fixed errors on shop.php

--- products/new.php
+++ products/new.php
@@ -1,3 +1,192 @@
 http://localhost:8080/project01/freshcery/products/detail-product.php?id=1
 
 http://localhost:8080/project01/freshcery/products/detail-product.php
+
+<section id="most-wanted">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="title">Most Wanted</h2>
+                        <div class="product-carousel owl-carousel">
+                        <?php foreach($allmostProducts as $allmostProduct) : ?>
+                            <div class="item">
+                                <div class="card card-product">
+                                    <div class="card-ribbon">
+                                        <div class="card-ribbon-container right">
+                                            <span class="ribbon ribbon-primary">SPECIAL</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="card-badge">
+                                        <div class="card-badge-container left">
+                                            <span class="badge badge-default">
+                                                Until <?php echo $allmostProduct->exp_date; ?>
+                                            </span>
+                                            <span class="badge badge-primary">
+                                                20% OFF
+                                            </span>
+                                        </div>
+                                        <img src="assets/img/<?php echo $allmostProduct->image; ?>" alt="Card image 2" class="card-img-top">
+                                    </div>
+
+                                    <div class="card-body">
+                                        <h4 class="card-title">
+                                            <a href="detail-product.php"><?php echo $allmostProduct->title; ?></a>
+                                        </h4>
+                                        <div class="card-price">
+                                           <!-- <span class="discount">Rp. 300.000</span> -->
+                                            <span class="reguler">€<?php echo $allmostProduct->price; ?></span>
+                                        </div>
+                                        <a href="detail-product.php" class="btn btn-block btn-primary">
+                                            Add to Cart
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                          <?php endforeach; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+<section id="meats">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="title">Meats</h2>
+                        <div class="product-carousel owl-carousel">
+                        <?php foreach($allMeats as $meat):?>
+                            <div class="item">
+                                <div class="card card-product">
+                                    <div class="card-ribbon">
+                                        <div class="card-ribbon-container right">
+                                            <span class="ribbon ribbon-primary">SPECIAL</span>
+                                        </div>
+                                    </div>
+                                    <div class="card-badge">
+                                        <div class="card-badge-container left">
+                                            <span class="badge badge-default">
+                                                Until <?php echo $meat->exp_date;?>
+                                            </span>
+                                            <span class="badge badge-primary">
+                                                20% OFF
+                                            </span>
+                                        </div>
+                                        <img src="assets/img/<?php echo $meat->image;?>" alt="Card image 2" class="card-img-top">
+                                    </div>
+                                    <div class="card-body">
+                                        <h4 class="card-title">
+                                            <a href="detail-product.php"><?php echo $meat->title;?></a>
+                                        </h4>
+                                        <div class="card-price">
+                                            <!--<span class="discount">Rp. 300.000</span> -->
+                                            <span class="reguler">€<?php echo $meat->price;?></span>
+                                        </div>
+                                        <a href="<?php echo APPURL; ?>products/detail-product.php?id=<?php echo $meat->id; ?>" class="btn btn-block btn-primary">
+                                            Add to Cart
+                                        </a>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="fishes" class="gray-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="title">Fishes</h2>
+                        <div class="product-carousel owl-carousel">
+                        <?php foreach($allFishes as $fish):?>
+                                <div class="item">
+                                    <div class="card card-product">
+                                        <div class="card-ribbon">
+                                            <div class="card-ribbon-container right">
+                                                <span class="ribbon ribbon-primary">SPECIAL</span>
+                                            </div>
+                                        </div>
+                                        <div class="card-badge">
+                                            <div class="card-badge-container left">
+                                                <span class="badge badge-default">
+                                                    Until <?php echo $fish->exp_date;?>
+                                                </span>
+                                                <span class="badge badge-primary">
+                                                    20% OFF
+                                                </span>
+                                            </div>
+                                            <img src="assets/img/<?php echo $fish->image;?>" alt="Card image 2" class="card-img-top">
+                                        </div>
+                                        <div class="card-body">
+                                            <h4 class="card-title">
+                                                <a href="detail-product.php"><?php echo $fish->title;?></a>
+                                            </h4>
+                                            <div class="card-price">
+                                                <!--<span class="discount">Rp. 300.000</span>-->
+                                                <span class="reguler">€<?php echo $fish->price;?></span>
+                                            </div>
+                                            <a href="<?php echo APPURL; ?>products/detail-product.php?id=<?php echo $fish->id; ?>" class="btn btn-block btn-primary">
+                                                Add to Cart
+                                            </a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="fruits">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="title">Fruits</h2>
+                        <div class="product-carousel owl-carousel">
+                        <?php foreach($allFruits as $fruit):?>  
+                            <div class="item">
+                                <div class="card card-product">
+                                    <div class="card-ribbon">
+                                        <div class="card-ribbon-container right">
+                                            <span class="ribbon ribbon-primary">SPECIAL</span>
+                                        </div>
+                                    </div>
+                                    <div class="card-badge">
+                                        <div class="card-badge-container left">
+                                            <span class="badge badge-default">
+                                                Until <?php echo $fruit->exp_date;?>
+                                            </span>
+                                            <span class="badge badge-primary">
+                                                20% OFF
+                                            </span>
+                                        </div>
+                                        <img src="assets/img/<?php echo $fruit->image;?>" alt="Card image 2" class="card-img-top">
+                                    </div>
+                                    <div class="card-body">
+                                        <h4 class="card-title">
+                                            <a href="detail-product.php"><?php echo $fruit->title;?></a>
+                                        </h4>
+                                        <div class="card-price">
+                                            <!--<span class="discount">Rp. 300.000</span>-->
+                                            <span class="reguler">€<?php echo $fruit->price;?></span>
+                                        </div>
+                                        <a href="<?php echo APPURL; ?>products/detail-product.php?id=<?php echo $fruit->id; ?>" class="btn btn-block btn-primary">
+                                            Add to Cart
+                                        </a>
+
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
--- shop.php
+++ shop.php
@@ -88,47 +88,49 @@
                     <div class="col-md-12">
                         <h2 class="title">Most Wanted</h2>
                         <div class="product-carousel owl-carousel">
-                        <?php foreach($allmostProducts as $allmostProduct) : ?>
-                            <div class="item">
-                                <div class="card card-product">
-                                    <div class="card-ribbon">
-                                        <div class="card-ribbon-container right">
-                                            <span class="ribbon ribbon-primary">SPECIAL</span>
+                            <?php foreach($allmostProducts as $allmostProduct) : ?>
+                                <div class="item">
+                                    <div class="card card-product">
+                                        <div class="card-ribbon">
+                                            <div class="card-ribbon-container right">
+                                                <span class="ribbon ribbon-primary">SPECIAL</span>
+                                            </div>
                                         </div>
-                                    </div>
+                                        <div class="card-badge">
+                                            <div class="card-badge-container left">
+                                                <span class="badge badge-default">
+                                                    Until <?php echo $allmostProduct->exp_date; ?>
+                                                </span>
+                                                <span class="badge badge-primary">
+                                                    20% OFF
+                                                </span>
+                                            </div>
+                                            <img src="assets/img/<?php echo $allmostProduct->image; ?>" alt="Card image 2" class="card-img-top">
+                                        </div>
+                                        <div class="card-body">
+                                            <h4 class="card-title">
+                                                <a href="detail-product.html"><?php echo $allmostProduct->title; ?></a>
+                                            </h4>
+                                            <div class="card-price">
+                                                <!-- <span class="discount">Rp. 300.000</span> -->
+                                                <span class="reguler"><?php echo $allmostProduct->price; ?>$</span>
+                                            </div>
+                                            <a href="<?php echo APPURL; ?>/products/detail-product.php?id=<?php echo $allmostProduct->id; ?>" class="btn btn-block btn-primary">
+                                                Add to Cart
+                                            </a>
 
-                                    <div class="card-badge">
-                                        <div class="card-badge-container left">
-                                            <span class="badge badge-default">
-                                                Until <?php echo $allmostProduct->exp_date; ?>
-                                            </span>
-                                            <span class="badge badge-primary">
-                                                20% OFF
-                                            </span>
                                         </div>
-                                        <img src="assets/img/<?php echo $allmostProduct->image; ?>" alt="Card image 2" class="card-img-top">
-                                    </div>
-
-                                    <div class="card-body">
-                                        <h4 class="card-title">
-                                            <a href="detail-product.html"><?php echo $allmostProduct->title; ?></a>
-                                        </h4>
-                                        <div class="card-price">
-                                           <!-- <span class="discount">Rp. 300.000</span> -->
-                                            <span class="reguler">€<?php echo $allmostProduct->price; ?></span>
-                                        </div>
-                                        <a href="detail-product.html" class="btn btn-block btn-primary">
-                                            Add to Cart
-                                        </a>
                                     </div>
                                 </div>
-                            </div>
-                          <?php endforeach; ?>
+                            <?php endforeach; ?>
+
                         </div>
                     </div>
                 </div>
             </div>
         </section>
+
+       
 
 
 
@@ -182,13 +184,13 @@
             </div>
         </section>
 
-        <section id="meats">
+        <section id="meats" class="gray-bg">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
                         <h2 class="title">Meats</h2>
                         <div class="product-carousel owl-carousel">
-                        <?php foreach($allMeats as $meat):?>
+                        <?php foreach($allMeats as $meat) : ?>
                             <div class="item">
                                 <div class="card card-product">
                                     <div class="card-ribbon">
@@ -199,29 +201,30 @@
                                     <div class="card-badge">
                                         <div class="card-badge-container left">
                                             <span class="badge badge-default">
-                                                Until <?php echo $meat->exp_date;?>
+                                            Until <?php echo $meat->exp_date; ?>
                                             </span>
                                             <span class="badge badge-primary">
                                                 20% OFF
                                             </span>
                                         </div>
-                                        <img src="assets/img/<?php echo $meat->image;?>" alt="Card image 2" class="card-img-top">
+                                        <img src="assets/img/<?php echo $meat->image; ?>" alt="Card image 2" class="card-img-top">
                                     </div>
                                     <div class="card-body">
                                         <h4 class="card-title">
-                                            <a href="detail-product.php"><?php echo $meat->title;?></a>
+                                            <a href="detail-product.php"><?php echo $meat->title; ?></a>
                                         </h4>
                                         <div class="card-price">
                                             <!--<span class="discount">Rp. 300.000</span> -->
-                                            <span class="reguler">€<?php echo $meat->price;?></span>
+                                            <span class="reguler">€<?php echo $meat->price; ?></span>
                                         </div>
-                                        <a href="<?php echo APPURL; ?>products/detail-product.php?id=<?php echo $meat->id; ?>" class="btn btn-block btn-primary">
+                                        <a href="<?php echo APPURL; ?>/products/detail-product.php?id=<?php echo $meat->id; ?>" class="btn btn-block btn-primary">
                                             Add to Cart
                                         </a>
 
                                     </div>
                                 </div>
                             </div>
+
                             <?php endforeach; ?>
                         </div>
                     </div>
@@ -229,13 +232,13 @@
             </div>
         </section>
 
-        <section id="fishes" class="gray-bg">
+        <section id="fishes">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
                         <h2 class="title">Fishes</h2>
                         <div class="product-carousel owl-carousel">
-                        <?php foreach($allFishes as $fish):?>
+                           <?php foreach($allFishes as $fish) : ?> 
                                 <div class="item">
                                     <div class="card card-product">
                                         <div class="card-ribbon">
@@ -246,35 +249,35 @@
                                         <div class="card-badge">
                                             <div class="card-badge-container left">
                                                 <span class="badge badge-default">
-                                                    Until <?php echo $fish->exp_date;?>
+                                                    Until <?php echo $fish->exp_date; ?>
                                                 </span>
                                                 <span class="badge badge-primary">
                                                     20% OFF
                                                 </span>
                                             </div>
-                                            <img src="assets/img/<?php echo $fish->image;?>" alt="Card image 2" class="card-img-top">
+                                            <img src="assets/img/<?php echo $fish->image; ?>" alt="Card image 2" class="card-img-top">
                                         </div>
                                         <div class="card-body">
                                             <h4 class="card-title">
-                                                <a href="detail-product.html"><?php echo $fish->title;?></a>
+                                                <a href="detail-product.php"><?php echo $fish->title; ?></a>
                                             </h4>
                                             <div class="card-price">
-                                                <!--<span class="discount">Rp. 300.000</span>-->
-                                                <span class="reguler">€<?php echo $fish->price;?></span>
+                                                <!-- <span class="discount">Rp. 300.000</span> -->
+                                                <span class="reguler">$ <?php echo $fish->price; ?></span>
                                             </div>
-                                            <a href="<?php echo APPURL; ?>products/detail-product.php?id=<?php echo $fish->id; ?>" class="btn btn-block btn-primary">
+                                            <a href="<?php echo APPURL; ?>/products/detail-product.php?id=<?php echo $fish->id; ?>" class="btn btn-block btn-primary">
                                                 Add to Cart
                                             </a>
 
                                         </div>
                                     </div>
                                 </div>
-                            <?php endforeach; ?>
+                          <?php endforeach; ?>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </section>   
 
         <section id="fruits">
             <div class="container">
@@ -282,45 +285,45 @@
                     <div class="col-md-12">
                         <h2 class="title">Fruits</h2>
                         <div class="product-carousel owl-carousel">
-                        <?php foreach($allFruits as $fruit):?>  
-                            <div class="item">
-                                <div class="card card-product">
-                                    <div class="card-ribbon">
-                                        <div class="card-ribbon-container right">
-                                            <span class="ribbon ribbon-primary">SPECIAL</span>
+                           <?php foreach($allFruits as $fruit) : ?> 
+                                <div class="item">
+                                    <div class="card card-product">
+                                        <div class="card-ribbon">
+                                            <div class="card-ribbon-container right">
+                                                <span class="ribbon ribbon-primary">SPECIAL</span>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="card-badge">
-                                        <div class="card-badge-container left">
-                                            <span class="badge badge-default">
-                                                Until <?php echo $fruit->exp_date;?>
-                                            </span>
-                                            <span class="badge badge-primary">
-                                                20% OFF
-                                            </span>
+                                        <div class="card-badge">
+                                            <div class="card-badge-container left">
+                                                <span class="badge badge-default">
+                                                    Until <?php echo $fruit->exp_date; ?>
+                                                </span>
+                                                <span class="badge badge-primary">
+                                                    20% OFF
+                                                </span>
+                                            </div>
+                                            <img src="assets/img/<?php echo $fruit->image; ?>" alt="Card image 2" class="card-img-top">
                                         </div>
-                                        <img src="assets/img/<?php echo $fruit->image;?>" alt="Card image 2" class="card-img-top">
-                                    </div>
-                                    <div class="card-body">
-                                        <h4 class="card-title">
-                                            <a href="detail-product.php"><?php echo $fruit->title;?></a>
-                                        </h4>
-                                        <div class="card-price">
-                                            <!--<span class="discount">Rp. 300.000</span>-->
-                                            <span class="reguler">€<?php echo $fruit->price;?></span>
-                                        </div>
-                                        <a href="<?php echo APPURL; ?>products/detail-product.php?id=<?php echo $fruit->id; ?>" class="btn btn-block btn-primary">
-                                            Add to Cart
-                                        </a>
+                                        <div class="card-body">
+                                            <h4 class="card-title">
+                                                <a href="detail-product.php"><?php echo $fruit->title; ?></a>
+                                            </h4>
+                                            <div class="card-price">
+                                                <!-- <span class="discount">Rp. 300.000</span> -->
+                                                <span class="reguler">$ <?php echo $fruit->price; ?></span>
+                                            </div>
+                                            <a href="<?php echo APPURL; ?>/products/detail-product.php?id=<?php echo $fruit->id; ?>" class="btn btn-block btn-primary">
+                                                Add to Cart
+                                            </a>
 
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        <?php endforeach; ?>
+                          <?php endforeach; ?>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </section>   
     </div>
     <?php require "includes/footer.php" ;?>
