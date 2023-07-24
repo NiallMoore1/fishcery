8a1f43ca6c28595cd868602e9c09f1fd4e21035d
NiallMoore <moore.nialls@gmail.com>
2023-07-24 23:54:29 +0100

    adding success.php page

--- products/success.php
+++ products/success.php
@@ -1,6 +1,29 @@
 <?php require "../includes/header.php"; ?>
-    <?php require "../config/config.php";  ?>
+<?php require "../config/config.php"; ?>
+<?php
 
-    <?php  echo "payment has been a success";?>
+if(isset($_SESSION['user_id'])) {
+    $delete = $conn->prepare("DELETE FROM cart WHERE user_id='$_SESSION[user_id]'");
+    $delete->execute();
 
-    <?php require "../includes/footer.php"; ?>
+}
+
+
+?>
+<div class="banner">
+            <div class="jumbotron jumbotron-bg text-center rounded-0" style="background-image: url('<?php echo APPURL; ?>/assets/img/bg-header.jpg');">
+                <div class="container">
+                    <h1 class="pt-5">
+                        Payment has been a success 
+                    </h1>
+                    <p class="lead">
+                        Save time and leave the groceries to us.
+                    </p>
+                    <a href="<?php echo APPURL; ?>" class="btn btn-primary text-uppercase">home</a>
+
+                  
+                </div>
+            </div>
+</div>
+
+<?php require "../includes/footer.php"; ?>
